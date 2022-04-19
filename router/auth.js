f26409bac935bf827d75669cc91868bbe66db051
minhyuk <minhyuk2@kakao.com>
2022-04-19 20:37:48 +0900

    first commit

---
+++ router/auth.js
@@ -0,0 +1,15 @@
+import express from "express";
+import * as authController from '../controller/auth.js';
+import { isAuth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post('/', authController.singup);
+router.put('/', authController.login);
+router.put('/auto', authController.autoLogin);
+router.get('/', isAuth, authController.getUserInfo);
+router.get('/terms', authController.getAuthTerms);
+router.get('/admin', isAuth, authController.adminGetUser);
+router.delete('/', isAuth, authController.logout);
+router.post('/sms', authController.sendsms);
+export default router;
